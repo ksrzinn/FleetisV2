00070c5547cb2a5832fad31527a7cfe3bbcb4983
Arthur <arthur.fortes@yardex.com.br>
2026-04-30 19:36:44 -0300

    fix: removendo rota do laravel

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,6 @@ if [ -z "$(ls -A /var/www/html/public-volume 2>/dev/null)" ]; then
     cp -r /var/www/html/public/. /var/www/html/public-volume/
 fi
 
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 exec php-fpm
--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,19 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified', 'tenant'])->name('dashboard');
 
 Route::middleware(['auth', 'verified', 'tenant'])->group(function () {
+    Route::get('/', DashboardController::class)->name('homepage');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
