a0232ac529c24fa575a1de5a8aa6fb6eef5a60b9
Arthur <arthur.fortes@yardex.com.br>
2026-05-01 17:53:21 -0300

    produção quenbrada

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ -z "$(ls -A /var/www/html/public-volume 2>/dev/null)" ]; then
-    cp -r /var/www/html/public/. /var/www/html/public-volume/
-fi
+cp -r /var/www/html/public/. /var/www/html/public-volume/
 
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
